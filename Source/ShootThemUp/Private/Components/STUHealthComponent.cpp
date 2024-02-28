73e5d493f680110511031e41ec17d44022456438
Kirill Trezubov <kirill3zoff@gmail.com>
2024-02-28 18:01:16 +0500

    36 lesson - Health | Damage type

--- Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "Components/STUHealthComponent.h"
 #include "GameFramework/Actor.h"
+#include "Dev/STUFireDamageType.h"
+#include "Dev/STUIceDamageType.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All)
 
@@ -36,4 +38,16 @@ void USTUHealthComponent::OnTakeAnyDamage(
 {
     Health -= Damage;
     UE_LOG(LogHealthComponent, Display, TEXT("Damage: %f"), Damage);
+
+	if (DamageType)
+    {
+        if (DamageType->IsA<USTUFireDamageType>())
+        {
+            UE_LOG(LogHealthComponent, Display, TEXT("So hooooooooooooooooooooot !!!!!!"));
+        }
+        else if (DamageType->IsA<USTUIceDamageType>())
+        {
+            UE_LOG(LogHealthComponent, Display, TEXT("So cold !!!!!!"));
+        }
+    }
 }
