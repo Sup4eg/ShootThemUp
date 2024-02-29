ee11adf9737ed4eeacdcf6a5757b251101b36ff5
Kirill Trezubov <kirill3zoff@gmail.com>
2024-02-29 13:48:53 +0500

    39 lesson - Health | Auto heal

--- Source/ShootThemUp/Public/Components/STUHealthComponent.h
+++ Source/ShootThemUp/Public/Components/STUHealthComponent.h
@@ -22,7 +22,7 @@ public:
     float GetHealth() const { return Health; }
 
     UFUNCTION(BlueprintCallable)
-    bool IsDead() const { return Health <= 0.0; }
+    bool IsDead() const { return FMath::IsNearlyZero(Health); }
 
     FOnDeath OnDeath;
     FOnHealthChanged OnHealthChanged;
@@ -31,13 +31,31 @@ protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Health", meta = (ClampMin = "0", ClampMax = "1000"))
     float MaxHealth = 100.0f;
 
+    
+    UPROPERTY(EditDefaultsOnly, Category = "Heal", meta = (EditCondition = "AutoHeal", ClampMin = "0", ClampMax = "10"))
+    float HealUpdateTime = 1.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Heal", meta = (EditCondition = "AutoHeal", ClampMin = "0", ClampMax = "10"))
+    float HealDelay = 3.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Heal", meta = (EditCondition = "AutoHeal", ClampMin = "1", ClampMax = "1000"))
+    float HealModifier = 1.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Heal")
+    bool AutoHeal = true;
+
+
     // Called when the game starts
     virtual void BeginPlay() override;
+
+private:
+    float Health = 0.0f;
+    FTimerHandle HealTimerHandle;
 
     UFUNCTION()
     void OnTakeAnyDamage(
         AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
 
-private:
-    float Health = 0.0f;
+    void HealUpdate();
+    void SetHealth(float NewHealth);
 };
