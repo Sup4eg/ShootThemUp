bc1f487e2a363e2e126a49fd55afb60880a2fc10
Kirill Trezubov <kirill3zoff@gmail.com>
2024-03-11 10:18:31 +0500

    75 lesson - Pickups | Respawn

--- Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -1,6 +1,5 @@
 // Shoot Them Up Game, All Rights Reserved
 
-
 #include "Pickups/STUBasePickup.h"
 #include "Components/SphereComponent.h"
 
@@ -8,32 +7,54 @@ DEFINE_LOG_CATEGORY_STATIC(LogBasePickup, All, All)
 
 ASTUBasePickup::ASTUBasePickup()
 {
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = true;
 
-	CollisionComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+    CollisionComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
     CollisionComponent->InitSphereRadius(50.0f);
     CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
     CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
     SetRootComponent(CollisionComponent);
-
 }
 
 void ASTUBasePickup::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+    check(CollisionComponent);
 }
 
 void ASTUBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
 {
     Super::NotifyActorBeginOverlap(OtherActor);
-    UE_LOG(LogBasePickup, Display, TEXT("Pickup was taken"));
-    Destroy();
+
+    const auto Pawn = Cast<APawn>(OtherActor);
+    if (GivePickupTo(Pawn))
+    {
+        PickupWasTaken();
+    }
 }
 
 void ASTUBasePickup::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 }
 
+bool ASTUBasePickup::GivePickupTo(APawn* PlayerPawn)
+{
+    return false;
+}
 
+void ASTUBasePickup::PickupWasTaken()
+{
+    CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+    if (!GetRootComponent()) return;
+    GetRootComponent()->SetVisibility(false, true);
+    FTimerHandle RespawnTimerHandle;
+    GetWorldTimerManager().SetTimer(RespawnTimerHandle, this, &ASTUBasePickup::Respawn, RespawnTime);
+}
+
+void ASTUBasePickup::Respawn()
+{
+    CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+    if (!GetRootComponent()) return;
+    GetRootComponent()->SetVisibility(true, true);
+}
