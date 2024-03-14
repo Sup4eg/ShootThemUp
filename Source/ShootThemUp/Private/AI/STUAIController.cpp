0b5d8cae1f88ecce83d829d436e88be4b4a31865
Kirill Trezubov <kirill3zoff@gmail.com>
2024-03-14 15:08:17 +0500

    97 lesson - AI | Perception component

--- Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -3,14 +3,26 @@
 
 #include "AI/STUAIController.h"
 #include "AI/STUAICharacter.h"
+#include "Components/STUAIPerceptionComponent.h"
 
+
+ ASTUAIController::ASTUAIController() {
+     STUAIPerceptionComponent = CreateDefaultSubobject<USTUAIPerceptionComponent>("STUPerceptionComponent");
+     SetPerceptionComponent(*STUAIPerceptionComponent);
+ }
 
 void ASTUAIController::OnPossess(APawn* InPawn) {
     Super::OnPossess(InPawn);
 
     const auto STUCharacter = Cast<ASTUAICharacter>(InPawn);
     if (STUCharacter)
-    {
+    {   
         RunBehaviorTree(STUCharacter->BehaviorTreeAsset);
     }
+}
+
+void ASTUAIController::Tick(float DeltaTime) {
+    Super::Tick(DeltaTime);
+    const auto AimActor = STUAIPerceptionComponent->GetClosestEnemy();
+    SetFocus(AimActor);
 }
