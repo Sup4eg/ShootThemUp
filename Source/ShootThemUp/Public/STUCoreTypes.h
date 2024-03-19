4b13abb84b0353794ce1f6b10e9aa8e636f4d0e1
Kirill Trezubov <kirill3zoff@gmail.com>
2024-03-19 10:32:05 +0500

    111 lesson - GameMode | Spawn AI bots

--- Source/ShootThemUp/Public/STUCoreTypes.h
+++ Source/ShootThemUp/Public/STUCoreTypes.h
@@ -86,3 +86,13 @@ struct FImpactData
     FDecalData DecalData;
 };
 
+
+USTRUCT(BlueprintType)
+struct FGameData
+{
+    GENERATED_USTRUCT_BODY()
+
+
+    UPROPERTY(EditDefaultsOnly, BluePrintReadWrite, Category = "Game", meta = (ClampMin = "1", ClampMax = "100"))
+    int32 PlayersNum = 2;
+};
