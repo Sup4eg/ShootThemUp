8a861418cfb5a10d6f4b7a7dcee4a7ff3af4b2ea
Kirill Trezubov <kirill3zoff@gmail.com>
2024-03-19 20:52:42 +0500

    116 lesson - GameMode | Stat: kills, deaths

--- Source/ShootThemUp/Private/Components/STUAIPerceptionComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUAIPerceptionComponent.cpp
@@ -24,7 +24,10 @@ AActor* USTUAIPerceptionComponent::GetClosestEnemy() const
     for (const auto PercieveActor : PercieveActors)
     {
         const auto HealthComponent = STUUtils::GetSTUPlayerComponent<USTUHealthComponent>(PercieveActor);
-        if (HealthComponent && !HealthComponent->IsDead()) // TODO: check if enemy or not
+        const auto PercievePawn = Cast<APawn>(PercieveActor);
+        const auto AreEnemies = PercievePawn && STUUtils::AreEnemies(Controller, PercievePawn->Controller);
+
+        if (HealthComponent && !HealthComponent->IsDead() && AreEnemies)
         {
             const auto CurrentDistance = (PercieveActor->GetActorLocation() - Pawn->GetActorLocation()).Size();
             if (CurrentDistance < BestDistance)
