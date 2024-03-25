ca68f41e801fe8d3a4c4a458020af5f72ffef1d2
Kirill Trezubov <kirill3zoff@gmail.com>
2024-03-25 10:22:43 +0500

    128 lesson - GameMenu | Game over, game stat widgets

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -115,8 +115,9 @@ void ASTUGameModeBase::CreateTeamsInfo()
 
         PlayerState->SetTeamID(TeamID);
         PlayerState->SetTeamColor(DetermineColorByTeamID(TeamID));
-
+        PlayerState->SetPlayerName(Controller->IsPlayerController() ? "Player" : "Bot");
         SetPlayerColor(Controller);
+
         TeamID = TeamID == 1 ? 2 : 1;
     }
 }
