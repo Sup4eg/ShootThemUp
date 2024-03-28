140f1c4ba3e77247a074d5c57c16c2dc72a80064
Kirill Trezubov <kirill3zoff@gmail.com>
2024-03-28 19:58:55 +0500

    143 lesson - Sound | Show widget, start game sounds

--- Source/ShootThemUp/Private/UI/STUBaseWidget.cpp
+++ Source/ShootThemUp/Private/UI/STUBaseWidget.cpp
@@ -2,7 +2,10 @@
 
 
 #include "UI/STUBaseWidget.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/GameplayStatics.h"
 
 void USTUBaseWidget::Show() {
     PlayAnimation(ShowAnimation);
+    UGameplayStatics::PlaySound2D(GetWorld(), OpenSound);
 }
--- Source/ShootThemUp/Public/Menu/UI/STUMenuWidget.h
+++ Source/ShootThemUp/Public/Menu/UI/STUMenuWidget.h
@@ -12,6 +12,7 @@ class UButton;
 class UHorizontalBox;
 class USTUGameInstance;
 class USTULevelItemWidget;
+class USoundCue;
 
 UCLASS()
 class SHOOTTHEMUP_API USTUMenuWidget : public USTUBaseWidget
@@ -33,6 +34,9 @@ protected:
 
     UPROPERTY(Transient, meta = (BindWidgetAnim))
     UWidgetAnimation* HideAnimation;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Sound")
+    USoundCue* StartGameSound;
 
     virtual void NativeOnInitialized() override;
     virtual void OnAnimationFinished_Implementation(const UWidgetAnimation* Animation) override;
