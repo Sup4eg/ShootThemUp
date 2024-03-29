a98ef18d4f78d0b3efd83ce67d9989b4082a108f
Kirill Trezubov <kirill3zoff@gmail.com>
2024-03-29 15:59:46 +0500

    148 lesson - Sound | Fire, no ammo, death sounds

--- Source/ShootThemUp/Private/Weapon/STURifleWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STURifleWeapon.cpp
@@ -105,7 +105,7 @@ void ASTURifleWeapon::SetFXActive(bool IsActive)
 
     if (FireAudioComponent)
     {
-        IsActive ? FireAudioComponent->Play() : FireAudioComponent->Stop();
+        FireAudioComponent->SetPaused(!IsActive);
     }
 }
 
